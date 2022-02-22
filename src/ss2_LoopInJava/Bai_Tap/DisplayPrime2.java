1c370a4aba116414b9333e9d04557e6b97f6dcbe
lehungson0510 <hungsonle0510@gmail.com>
2022-02-22 10:29:05 +0700

    up bt

---
+++ src/ss2_LoopInJava/Bai_Tap/DisplayPrime2.java
@@ -0,0 +1,21 @@
+package ss2_LoopInJava.Bai_Tap;
+
+public class DisplayPrime2 {
+    public static void main(String[] args) {
+        int number = 2;
+        while (number < 100) {
+            boolean isPrime=true;
+            for(int i=2;i<=Math.sqrt(number);i++){
+                if(number%i==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.print( " "+number+" " );
+                
+            }
+            number++;
+        }
+    }
+}
